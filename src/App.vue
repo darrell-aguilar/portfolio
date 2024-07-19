4ac5e9ba1d2f1dccd559ba8823f5dfcc706c6783
darrell404 <darrell029@gmail.com>
2024-07-19 01:54:39 +0100

    initial setup commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <Intro />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import Intro from "@/components/Intro.vue"
+
+export default defineComponent({
+  components: {
+    Intro,
+  },
+})
+</script>
+
+<style scoped>
+.app {
+  padding: 0 6rem;
+}
+</style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="intro">
+    <h1 class="intro_title">
+      <a href="/">Darrell Aguilar</a>
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "Intro",
+})
+</script>
+
+<style scoped lang="scss">
+.intro {
+  &_title {
+    font-size: 3rem;
+  }
+}
+</style>
